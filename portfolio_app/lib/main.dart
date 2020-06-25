559aad8bb65397cfe3ec9e8ad787769e57fa821f
Sandesh Sharma <iammandesh@gmail.com>
2020-06-25 17:34:55 +0545

    Simple UI page

--- portfolio_app/lib/main.dart
+++ portfolio_app/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
               backgroundImage: AssetImage('images/sandesh.jpg'),
             ),
             Text(
-              'Sandesh Sharma',
+              'Sandesh',
               style: TextStyle(
                 fontFamily: 'Pacifico',
                 fontSize: 40.0,
